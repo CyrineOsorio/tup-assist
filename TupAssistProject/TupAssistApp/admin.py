fd84fa70889a107503b246ac87f1d55f2f3c72a8
Rejay Morada <rejaymorada0@gmail.com>
2022-10-24 12:42:50 +0800

    Add Files

--- TupAssistProject/TupAssistApp/admin.py
+++ TupAssistProject/TupAssistApp/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 
+from .models import registration
+
 # Register your models here.
+# https://docs.djangoproject.com/en/1.8/intro/tutorial02/
 
 
+admin.site.register(registration)
 

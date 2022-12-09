5b5a3f66bf30c8c54c2ba76f1fc80c5ae06c66ab
unknown <rejaymorada0@gmail.com>
2022-12-09 16:16:40 +0800

    Add Files

--- TupAssistProject/TupAssistApp/views.py
+++ TupAssistProject/TupAssistApp/views.py
@@ -1137,14 +1137,60 @@ def h_dropping(request):
 def h_dropping_edit(request, studID):
     current_user = request.user
     data = registration.objects.get(studID=studID)
-    req = DroppingReq.objects.filter(studID_id=data.studID)
     sched = Schedule.objects.all()
-    context = { 
-        'current_user': current_user,
-        'student_info': data,
-        'req': req,
-        'sched': sched
-        }
+    if current_user.department == "Department of Industrial Technology":
+        department1 = "DIT"
+        course1 = "BET-COET"
+        req = DroppingReq.objects.filter(Q (studID_id=data.studID) & (Q(subject_id__course=department1) | (Q(subject_id__course=course1))))
+        context = { 
+            'current_user': current_user,
+            'student_info': data,
+            'req': req,
+            'sched': sched
+            }
+    elif current_user.department == "Department of Information Education":
+        course1 = "BS-ICT"
+        req = DroppingReq.objects.filter(Q(studID_id=data.studID) & (Q(subject_id__course=course1)))
+        context = { 
+            'current_user': current_user,
+            'student_info': data,
+            'req': req,
+            'sched': sched
+            }
+    elif current_user.department == "Department of Engineering":
+        course1 = "BSCE"
+        req = DroppingReq.objects.filter(Q(studID_id=data.studID) & (Q(subject_id__course=course1)))
+        context = { 
+            'current_user': current_user,
+            'student_info': data,
+            'req': req,
+            'sched': sched
+            }
+    elif current_user.department == "Department of Math and Science":
+        department1 = "DMS"
+        req = DroppingReq.objects.filter(Q(studID_id=data.studID) & (Q(subject_id__course=department1)))
+        context = { 
+            'current_user': current_user,
+            'student_info': data,
+            'req': req,
+            'sched': sched
+            }
+    elif current_user.department == "Department of Liberal Arts":
+        department1 = "DLA"
+        req = DroppingReq.objects.filter(Q(studID_id=data.studID) & (Q(subject_id__course=department1)))
+        context = { 
+            'current_user': current_user,
+            'student_info': data,
+            'req': req,
+            'sched': sched
+            }
+    else: 
+        context = { 
+            'current_user': current_user,
+            'student_info': data,
+            'req': req,
+            'sched': sched
+            }
     return render(request, 'TupAssistApp/h_dropping_edit.html', context)
 
 def h_edit_sub1(request):

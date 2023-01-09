edf80aeba79b16ef491d3a8190c7a0a8d6477d32
Rejay Morada <rejaymorada0@gmail.com>
2023-01-09 15:02:19 +0800

    Add Files

--- TupAssistProject/TupAssistApp/views.py
+++ TupAssistProject/TupAssistApp/views.py
@@ -1567,13 +1567,29 @@ def h_adding(request):
         current_user = request.user
         if current_user.department == "Department of Industrial Technology" or current_user.department == "Department of Engineering" or current_user.department == "Department of Industrial Education":
             test = registration.objects.filter(Q(department=current_user.department) & Q(userType='Student') & (~Q(addStatus='')))
+            cnt = len(AddingReq.objects.filter(head_is_approve='Pending'))
+            cnt1 = len(AddingReq.objects.filter(Q(studID__department=current_user.department) & Q(head_is_approve='Decline')))
+            cnt2 = len(AddingReq.objects.filter(Q(studID__department=current_user.department) & Q(head_is_approve='Pending')))
+            cnt3 = len(AddingReq.objects.filter(Q(studID__department=current_user.department) & Q(head_is_approve='Approved')))
             context = { 
+                'cnt': cnt,
+                'cnt1': cnt1,
+                'cnt2': cnt2,
+                'cnt3': cnt3,
                 'test': test,
                 'current_user': current_user
                 }
         else:
             test = registration.objects.filter(Q(userType='Student') & (~Q(addStatus='')))
+            cnt = len(AddingReq.objects.filter(head_is_approve='Pending'))
+            cnt1 = len(AddingReq.objects.filter(Q(studID__department=current_user.department) & Q(head_is_approve='Decline')))
+            cnt2 = len(AddingReq.objects.filter(Q(studID__department=current_user.department) & Q(head_is_approve='Pending')))
+            cnt3 = len(AddingReq.objects.filter(Q(studID__department=current_user.department) & Q(head_is_approve='Approved')))
             context = { 
+                'cnt': cnt,
+                'cnt1': cnt1,
+                'cnt2': cnt2,
+                'cnt3': cnt3,
                 'test': test,
                 'current_user': current_user
                 }

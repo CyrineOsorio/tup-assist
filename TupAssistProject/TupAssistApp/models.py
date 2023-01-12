23537ad4350c21be840831517799d9ec9db68d60
Rejay Morada <rejaymorada0@gmail.com>
2023-01-12 11:33:39 +0800

    Add FIles

--- TupAssistProject/TupAssistApp/models.py
+++ TupAssistProject/TupAssistApp/models.py
@@ -11,6 +11,8 @@ class registration(AbstractUser):
         ('Department Head', 'Department Head'),
         ('Person-in-charge', 'Person-in-charge'), 
         ('Teacher', 'Teacher'), 
+        ('Assist. Director for Academic Affairs', 'Assist. Director for Academic Affairs'), 
+        ('OAA Staff', 'OAA Staff'),
     ]
 
     department = [
